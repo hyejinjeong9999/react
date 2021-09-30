b5fc2142d2503cec624f9106191482ec6345f90c
hyejinzz <hyejinj810@gmail.com>
2021-09-30 21:01:02 +0900

    리액트 기초 - event

--- basic/src/3_EventHandling/EventPractice_3.js
+++ basic/src/3_EventHandling/EventPractice_3.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
 /**
- * inputㅇ 여러개일 때 event 객체를 활용한다
+ * input이 여러개일 때 event 객체를 활용한다
  * e.target.name 값을 사용하면 된다
  * onChange 이벤트 핸들러에서 e.target.name 값을 사용하면 된다.
  *e.target.name 해당 인풋의 name을 가리킨다
